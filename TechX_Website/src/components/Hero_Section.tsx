c45e09ff1bb8b079165f6114dbe9ba691202e259
Sara <sarapsdp@gmail.com>
2025-09-08 13:38:27 -0400

    updated hero registration link

--- TechX_Website/src/components/Hero_Section.tsx
+++ TechX_Website/src/components/Hero_Section.tsx
@@ -10,10 +10,12 @@ export default function Hero_Section(){
             <div className={"absolute top-45 flex flex-col justify-center items-center z-2 gap-4"}>
                 <h1 className={"text-white text-4xl"}> TechX Florida 2025 </h1>
                 <p className={"text-white text-lg text-center"}> Innovate. Connect. Transform. Join us for the premier tech conference of the year. </p>
+            <a href="https://bullsconnect.usf.edu/ieeecs/rsvp_boot?id=1985102" target="_blank" >"
                 <button className={"w-[10rem] h-[4rem] bg-[#FFA300] rounded"}>
                     <h2 className={" font-bold text-[#00629B]"}> Register Now </h2>
 
                 </button>
+            </a>
 
             </div>
 
