cfc303d622cf9f580f21e210a9e6331897499117
Caio Carraro Bahlis <caiobahlis@eduroam-226-195-119.laptops.usf.edu>
2025-09-08 21:10:02 -0400

    Finished Mobile Layout

--- TechX_Website/src/components/Get_Tickets.tsx
+++ TechX_Website/src/components/Get_Tickets.tsx
@@ -7,8 +7,8 @@ export default function Get_Tickets(){
             <h2 className={"text-4xl text-white tracking-tight font-bold pt-[3rem]"}> Secure your Spot </h2>
 
             <p className={"w-[80%] text-lg text-white text-center text-balance pt-[2rem]"}> Don't miss out on the opportunity to be a part of TechX Florida 2025.
-                Expect actionable talks from practitioners, live product showcases, skill-building workshops,
-                and a talent-packed expo floor. Come to learn. Stay to connect. Leave with momentum.
+                Register today to secure your spot at this landmark event.
+
             </p>
 
               <button className={"w-[10rem] h-[4rem] bg-[#FFA300] rounded mt-[2rem]"}>
