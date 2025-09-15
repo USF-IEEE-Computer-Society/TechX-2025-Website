46a7fb8218276e0bcaf88e71abdb96ffd2191b72
Sara <sarapsdp@gmail.com>
2025-09-14 22:53:32 -0400

    add ieee-cs loo

--- TechX_Website/src/components/Navbar.tsx
+++ TechX_Website/src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 
 import logo from "../assets/logoTechX.jpeg"
+import logo2 from "../assets/ieee-cs-logo.png"
 
 export default function Navbar(){
 
     return (
        <div className={"w-screen h-[6rem] bg-white shadow-md flex items-center justify-between"}>
-              <img src={logo} alt="TechX 2025 Logo" className="h-full w-auto pl-[1rem]" />
+        <div className="flex items-center gap-4 pl-[1rem]">
+            <img src={logo} alt="TechX 2025 Logo" className="h-[4rem] w-auto" />
+            <img src={logo2} alt="IEEE CS Logo" className="h-[4rem] w-auto" />
+        </div>
 
            <div className={"w-[50px] h-[50px] flex flex-col gap-1 justify-center items-center pr-[1rem]"}>
                <div className={"w-[15px] h-[2px] bg-black"}/>
