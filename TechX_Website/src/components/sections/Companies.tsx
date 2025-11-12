6a22fddda5c40ec1cadbd057819f41889da034a9
Egor Kharitonov <ega.khar@gmail.com>
2025-11-11 19:54:16 -0500

    Adding java user group

--- TechX_Website/src/components/sections/Companies.tsx
+++ TechX_Website/src/components/sections/Companies.tsx
@@ -6,19 +6,23 @@ import CouchBaseImg from "../../../src/assets/companies/Couchbase_Logo.jpg"
 import USFITImg from "../../../src/assets/companies/information-technology-lightbg-2c-rgb-h.png"
 import uberaiImg from "../../../src/assets/companies/uberai.jpg"
 import icode from "../../../src/assets/companies/icode.png"
+import java from "../../../src/assets/companies/java.png"
 
 
 export default function Companies(){
 
     return (
         <div id={'Organizers'} className={"w-full h-auto bg-white flex flex-col items-center pb-[3rem] p-[1rem]"}>
+
             <div className={"w-full flex flex-col justify-center items-center gap-3 mb-10"}>
                 <h2 className={"text-4xl md:text-5xl tracking-tight font-bold pt-[3rem]"}>Attending Companies</h2>
                 <p className={"w-[90%] text-md md:text-2xl text-ellipsis text-gray-600 text-center text-pretty"}> Connect
                     with the companies tabling and showcasing opportunities at TechX Florida 2025</p>
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-4 md:gap-y-12 md:gap-x-8 gap-y-5 gap-x-5 place-items-center">
+            {/* Absolute garbage code down below, please dont judge me =D */}
+
+            <div className="grid grid-cols-2 md:grid-cols-4 md:gap-y-12 md:gap-x-8 gap-y-8 gap-x-5 place-items-center block md:hidden">
 
                 <a href="https://ironeaglex.com" target="_blank" rel="noopener noreferrer">
                     <img src={iexImg} className="h-16 md:h-32" loading="lazy"/>
@@ -39,16 +43,66 @@ export default function Companies(){
                 <a href="https://icodeschool.com/" target="_blank" rel="noopener noreferrer">
                     <img src={icode} className="h-16 md:h-32" loading="lazy"/>
                 </a>
+
+                <a href="https://www.tampajug.com/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
+                    <img src={java} className="h-16 md:h-32 object-contain" loading="lazy"/>
+                </a>
+
                 <a href="https://www.uber.com/us/en/ai-solutions/" target="_blank" rel="noopener noreferrer">
                     <img src={uberaiImg} className="h-16 md:h-32" loading="lazy"/>
                 </a>
 
+
+            </div>
+
+            
+            <div className="block md:hidden items-center justify-center mt-8">
                 <a href="https://www.tampadevs.com/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
                     <img src={tampadevsImg} className="h-16 md:h-32 object-contain" loading="lazy"/>
                 </a>
+            </div>
 
+            
 
+            <div className="md:flex md:flex-row md:gap-y-12 md:gap-x-8 hidden md:block mb-10">
+            <a href="https://ironeaglex.com" target="_blank" rel="noopener noreferrer">
+                    <img src={iexImg} className="h-16 md:h-32" loading="lazy"/>
+                </a>
+                <a href="https://www.verizon.com" target="_blank" rel="noopener noreferrer">
+                    <img src={verizonImg} className="h-16 md:h-32" loading="lazy"/>
+                </a>
 
+                <a href="https://www.westfloridaisaca.org/" target="_blank" rel="noopener noreferrer">
+                    <img src={isacaImg} className="h-16 md:h-32" loading="lazy"/>
+                </a>
+                <a href="https://www.couchbase.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={CouchBaseImg} className="h-16 md:h-32" loading="lazy"/>
+                </a>
+            </div>
+
+            <div className="md:flex md:flex-row md:gap-y-12 md:gap-x-8 hidden md:block mb-10">
+                <a href="https://www.usf.edu/it/" target="_blank" rel="noopener noreferrer">
+                    <img src={USFITImg} className="h-16 md:h-32" loading="lazy"/>
+                </a>
+                <a href="https://icodeschool.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={icode} className="h-16 md:h-32" loading="lazy"/>
+                </a>
+
+                <a href="https://www.tampajug.com/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
+                    <img src={java} className="h-16 md:h-32 object-contain" loading="lazy"/>
+                </a>
+
+            </div>
+
+            <div className="md:flex md:flex-row md:gap-y-12 md:gap-x-8 hidden md:block">
+
+                <a href="https://www.tampadevs.com/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
+                        <img src={tampadevsImg} className="h-16 md:h-32 object-contain" loading="lazy"/>
+                    </a>
+
+                <a href="https://www.uber.com/us/en/ai-solutions/" target="_blank" rel="noopener noreferrer">
+                    <img src={uberaiImg} className="h-16 md:h-32" loading="lazy"/>
+                </a>
             </div>
 
 
